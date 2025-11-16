f3d14b7b297c544ac1047d193ea7babab5e4b2e7
SB2318 <sb2318@it.jgec.ac.in>
2025-11-16 15:26:32 +0530

    --up

--- controllers/aiChatController.js
+++ controllers/aiChatController.js
@@ -9,7 +9,7 @@ const Message = require('../models/ai/Message');
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 
 const startConversation = expressAsyncHandler(
